d081d0a106fb76754f6745f3c0f6d64b24be717f
Julian Nwadinobi <juliannwadinobi098@gmail.com>
2025-07-25 01:02:07 +0100

    Update streamHandler.js

--- src/handlers/streamHandler.js
+++ src/handlers/streamHandler.js
@@ -1,19 +1,11 @@
 import fs from "fs/promises";
 import path from "path";
-import { nanoid } from "nanoid";
-
 import { validateImageDimensions } from "../utils/imageValidator.utils.js";
 
 const __dirname = path.resolve();
 
 export async function handleStream(type) {
   if (type === "tv") {
-    const uniqueID = process.env.UNIQUE_ID;
-    const categoryIds = [
-      "67b052ba9748d1eacb867573",
-      "1234567890abcdef12345678"
-    ];
-
     const jsonPath = path.join(__dirname, "public", "streams.json");
     const fileData = await fs.readFile(jsonPath, "utf-8");
     const rawChannels = JSON.parse(fileData);
@@ -21,21 +13,18 @@ export async function handleStream(type) {
     const streams = [];
 
     for (const channel of rawChannels) {
-      const id = "1";
       const imageMeta = await validateImageDimensions(channel.imageUrl);
 
       streams.push({
-        id,
+        id: "1", 
         slug: channel.slug,
         name: channel.name,
         hash: "#Comedy",
-        number: 100,
-        summary: `Watch ${channel.name}`,
+        category: "1", 
+        summary: `Watch ${channel.name} - a Comedy channel.`,
         rating: "PG",
-        plutoOfficeOnly: false,
-        featured: false,
-        featuredOrder: -1,
-        isStitched: true,
+        image: channel.imageUrl || imageMeta?.url || "",
+        background: channel.background || "URL_ADDRESSghghg/jpg",
         stitched: {
           paths: [
             {
@@ -44,14 +33,11 @@ export async function handleStream(type) {
             },
           ],
         },
-        images: imageMeta ? [imageMeta] : [],
-        categoryIDs: categoryIds,
-        tmsid: null,
       });
     }
 
-    return { streams, categoryIds };
+    return streams;
   }
 
-  return { streams: [], categoryIds: [] };
+  return [];
 }
